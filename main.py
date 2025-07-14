65be949ea3c58656eafe9b652be8dc8be192d762
MattiWaki <wakiwaya.matti@gmail.com>
2025-07-14 19:31:46 +0000

    Added welcome message.

--- main.py
+++ main.py
@@ -3,5 +3,6 @@
 def main():
     print("Hello, welcome to our game!")
     print("We will bring you something exciting, soon!")
+    print("Enjoy your adventure!")
 if __name__ == "__main__":
     main()
